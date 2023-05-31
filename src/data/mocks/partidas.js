691194fb129d9ae79c774da2076251fd859adcce
Henrique Alves <henryworkdev.ti@gmail.com>
2023-05-31 13:12:42 -0300

    adicionando mock partidas

---
+++ src/data/mocks/partidas.js
@@ -0,0 +1,52 @@
+const partidas = {
+    rodadas:[
+        [
+            {
+               timeA: "INFOR 2º",
+               timeB: "AGRO 3º",
+               placarA: 3,
+               placarB: 2,
+               data:"18/05/2023",
+               hora:"12:15",
+               finalizado: true,
+               vencedor:"A",
+            },
+            {
+                timeA: "EDIF 2º",
+                timeB: "ADM",
+                placarA: 0,
+                placarB: 0,
+                data:"18/06/2023",
+                hora:"12:15",
+                finalizado: false,
+                vencedor:"",
+             },
+
+        ],
+        [
+            {
+               timeA: "INFOR 2º",
+               timeB: "AGRO 3º",
+               placarA: 3,
+               placarB: 2,
+               data:"18/05/2023",
+               hora:"12:15",
+               finalizado: true,
+               vencedor:"A",
+            },
+            {
+                timeA: "EDIF 2º",
+                timeB: "ADM",
+                placarA: 0,
+                placarB: 0,
+                data:"18/06/2023",
+                hora:"12:15",
+                finalizado: false,
+                vencedor:"",
+             },
+
+        ]
+    ]
+}
+
+export default partidas;
--- src/pages/partidas.tsx
+++ src/pages/partidas.tsx
@@ -1,10 +1,10 @@
-import Tabela from '@/components/Tabela'
 import Pagina from '@/components/template/Pagina'
 import Cabecalho from '@/components/template/Cabecalho'
 import Menu from '@/components/template/Menu'
 import MenuItem from '@/components/template/MenuItem'
 import CardRodada from '@/components/CardRodada'
 import Partida from '@/components/Partida'
+import partidas from "../data/mocks/partidas"
 
 export default function Partidas() {
   return (
@@ -22,50 +22,26 @@ export default function Partidas() {
           </MenuItem>
         </Menu>
       </Cabecalho>
-      <CardRodada rodada={1} totalRodadas={6}>
-        <Partida
-          timeA='INFOR 3º'
-          timeB='AGRO 2º'
-          placarA={3}
-          placarB={2}
-          finalizado={true}
-          data='18/05/2023'
-          hora='12:15'
-          vencedor='A'
-        />
-        <Partida
-          timeA='EDIF 1º'
-          timeB='AGRO 2º'
-          placarA={0}
-          placarB={0}
-          finalizado={false}
-          data='09/05/2023'
-          hora='12:15'
-          vencedor=''
-        />
-      </CardRodada>
-      <CardRodada rodada={2} totalRodadas={6}>
-        <Partida
-          timeA='INFOR 3º'
-          timeB='AGRO 2º'
-          placarA={3}
-          placarB={2}
-          finalizado={true}
-          data='18/05/2023'
-          hora='12:15'
-          vencedor='A'
-        />
-        <Partida
-          timeA='EDIF 1º'
-          timeB='AGRO 2º'
-          placarA={0}
-          placarB={0}
-          finalizado={false}
-          data='09/05/2023'
-          hora='12:15'
-          vencedor=''
-        />
-      </CardRodada>
+      {
+        partidas.rodadas.map((rodada, index) =>
+          <CardRodada rodada={index+1} totalRodadas={rodada.length}>
+            {
+                rodada.map(partida => 
+                  <Partida
+                    timeA={partida.timeA}
+                    timeB={partida.timeB}
+                    placarA={partida.placarA}
+                    placarB={partida.placarB}
+                    finalizado={partida.finalizado}
+                    data={partida.data}
+                    hora={partida.hora}
+                    vencedor={partida.vencedor}
+                  />
+                )
+            }
+          </CardRodada>
+        )
+      }
     </Pagina>
   )
 }
