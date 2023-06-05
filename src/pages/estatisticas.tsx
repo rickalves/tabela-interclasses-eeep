43043558cf4825848d9ccfbb2c85818c0af1363a
Henrique Alves <henryworkdev.ti@gmail.com>
2023-06-05 11:45:51 -0300

    ajustando o frontend

--- src/pages/estatisticas.tsx
+++ src/pages/estatisticas.tsx
@@ -5,12 +5,40 @@ import MenuItem from '@/components/template/MenuItem'
 import Card from '@/components/Card'
 import Jogador from '@/components/Jogador'
 import Rodape from '@/components/template/Rodape'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+interface GolsType{
+  _id?:any;
+  nome:string;
+  time:string;
+  gols:number;
+  foto:string 
+}
+
+interface Cartoes{
+  _id?:any;
+  nome:string;
+  time:string;
+  cartoes:number;
+  foto:string 
+}
 
 export default function Estatisticas() {
-  const [gols, setGols] = useState([]) 
-  const [cartoesAmarelos, setCartoesAmarelos] = useState([]) 
-  const [cartoesVermelhos, setCartoesVermelhos] = useState([]) 
+  const [gols, setGols] = useState(Array<GolsType>)
+  const [cartoesAmarelos, setCartoesAmarelos] = useState(Array<Cartoes>)
+  const [cartoesVermelhos, setCartoesVermelhos] = useState(Array<Cartoes>)
+
+  useEffect(() => {
+    fetch('https://api-interclasses-app.vercel.app/api/estatisticas')
+      .then(resp => resp.json())
+      .then(estatisticas => {
+        setGols(estatisticas.gols)
+        setCartoesAmarelos(estatisticas.cartoes_amarelos)
+        setCartoesVermelhos(estatisticas.cartoes_vermelhos)
+      })
+      .catch(err => console.log("erro ao carregar os dados", err))
+  }, [])
+
   return (
     <Pagina>
       <Cabecalho>
@@ -27,7 +55,7 @@ export default function Estatisticas() {
         </Menu>
       </Cabecalho>
       <Card titulo='Gols'>
-          {/* {gols.map((jogador, index) => 
+        {gols.map((jogador, index) => 
               <Jogador 
                 key={index}
                 nome={jogador.nome}
@@ -35,10 +63,10 @@ export default function Estatisticas() {
                 posicao={index + 1}
                 estatistica={jogador.gols}
                 foto={jogador.foto || 'foto-icon.svg'}
-              />)} */}
+              />)}
       </Card>
       <Card titulo='Cartões Amarelos'>
-          {/* {cartoesAmarelos.map((jogador, index) => 
+        {cartoesAmarelos.map((jogador, index) => 
               <Jogador 
                 key={index}
                 nome={jogador.nome}
@@ -46,10 +74,10 @@ export default function Estatisticas() {
                 posicao={index + 1}
                 estatistica={jogador.cartoes}
                 foto={jogador.foto || 'foto-icon.svg'}
-              />)} */}
+              />)}
       </Card>
       <Card titulo='Cartões Vermelhos'>
-          {/* {cartoesVermelhos.map((jogador, index) => 
+        {cartoesVermelhos.map((jogador, index) => 
               <Jogador 
                 key={index}
                 nome={jogador.nome}
@@ -57,7 +85,7 @@ export default function Estatisticas() {
                 posicao={index + 1}
                 estatistica={jogador.cartoes}
                 foto={jogador.foto || 'foto-icon.svg'}
-              />)} */}
+              />)}
       </Card>
       <Rodape />
     </Pagina>
--- src/pages/partidas.tsx
+++ src/pages/partidas.tsx
@@ -5,10 +5,35 @@ import MenuItem from '@/components/template/MenuItem'
 import CardRodada from '@/components/CardRodada'
 import Partida from '@/components/Partida'
 import Rodape from '@/components/template/Rodape'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+interface Partida{
+  _id?:any;
+  timeA:string;
+  timeB:string;
+  placarA:number;
+  placarB:number;
+  data:string;
+  hora:string;
+  finalizado:boolean
+  vencedor:string
+}
+
+
 
 export default function Partidas() {
-  const [partidas, setPartidas] = useState([])
+  const [partidas, setPartidas] = useState({
+      _id:"",
+      temporada:0,
+      rodadas:[[]]
+  })
+  useEffect(()=>{
+    fetch('https://api-interclasses-app.vercel.app/api/partidas')
+        .then(resp => resp.json())
+        .then(partidas => setPartidas(partidas))
+        .catch(err => console.log("erro ao carregar os dados", err))
+  },[])
+
   return (
     <Pagina>
       <Cabecalho>
@@ -25,26 +50,26 @@ export default function Partidas() {
         </Menu>
       </Cabecalho>
       {
-        // partidas.rodadas.map((rodada, index) =>
-        //   <CardRodada rodada={index+1} totalRodadas={rodada.length} key={index}>
-        //     {
-        //         rodada.map((partida, index) => 
-        //           <Partida
-        //             key={index}
-        //             timeA={partida.timeA}
-        //             timeB={partida.timeB}
-        //             placarA={partida.placarA}
-        //             placarB={partida.placarB}
-        //             finalizado={partida.finalizado}
-        //             data={partida.data}
-        //             hora={partida.hora}
-        //             vencedor={partida.vencedor}
-        //             colspan={index == rodada.length-1 && rodada.length == 5? true : false}
-        //           />
-        //         )
-        //     }
-        //   </CardRodada>
-        // )
+        partidas.rodadas.map((rodada, index) =>
+          <CardRodada rodada={index+1} totalRodadas={rodada.length} key={index}>
+            {
+                rodada.map((partida:Partida, index:number) => 
+                  <Partida
+                    key={index}
+                    timeA={partida.timeA}
+                    timeB={partida.timeB}
+                    placarA={partida.placarA}
+                    placarB={partida.placarB}
+                    finalizado={partida.finalizado}
+                    data={partida.data}
+                    hora={partida.hora}
+                    vencedor={partida.vencedor}
+                    colspan={index == rodada.length-1 && rodada.length == 5? true : false}
+                  />
+                )
+            }
+          </CardRodada>
+        )
       }
       <Rodape />
     </Pagina>
